edb286822620788d16ea504c9514005ac830bff8
Patr1arch <43929725+Patr1arch@users.noreply.github.com>
2020-10-17 00:09:05 +1000

    #1 Make parallel using common matrix

--- MPITest/Source.cpp
+++ MPITest/Source.cpp
@@ -6,8 +6,12 @@
 
 using namespace std;
 
+int algebraic_complement(int** data) {
+	return 0;
+}
+
 int main(int* argc, char** argv) {
-	int numtasks, rank;
+	int numtasks, rank, n;
 	//int* n;
 	//printf("Enter the order of the matrix\n");
 	//scanf("d", &n);
@@ -20,14 +24,14 @@ int main(int* argc, char** argv) {
 	//printf(argv[2]);
 
 	MPI_Init(argc, &argv);
+	MPI_Status st;
 
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-	int ** matrix;
+	int** matrix;
 
 	if (rank == 0) {
 		ifstream matrix_data("matrix_data.txt");
-		int n;
 		matrix_data >> n;
 		matrix = new int* [n];
 
@@ -41,9 +45,42 @@ int main(int* argc, char** argv) {
 		}
 
 		matrix_data.close();
+		// rank * (n / numtasks + n % numtasks)
+		//cout << (n / numtasks + n % numtasks) << endl;
+		//cout << numtasks << endl;
+		//cout << n << endl;
+		for (int p = 1; p < numtasks; p++)
+		{
+			MPI_Send(&n, 1, MPI_INT, p, 99, MPI_COMM_WORLD);
+			for (int k = 0; k < n; k++) {
+				MPI_Send(matrix[k], n, MPI_INT, p, 99, MPI_COMM_WORLD);
+			}
+		}
+
+		for (int i = (rank + 0) * (n / numtasks + n % numtasks); i < (rank + 1) * (n / numtasks + n % numtasks) && i < n; i++) {
+			cout << "Call " << i << "For rank " << rank << endl;
+		}
 
 		for (int i = 0; i < n; i++)
 			delete [] matrix[i];
+		delete[] matrix;
+	}
+	else {
+		MPI_Recv(&n, 1, MPI_INT, 0, 99, MPI_COMM_WORLD, &st);
+		cout << "Rank " << rank << "has element " << n << endl;
+		matrix = new int* [n];
+		for (int i = 0; i < n; i++) {
+			matrix[i] = new int[n];
+			MPI_Recv(matrix[i], n, MPI_INT, 0, 99, MPI_COMM_WORLD, &st);
+		}
+
+		cout << "Rank " << rank << "has matrix[rank][rank] element " << matrix[rank][rank] << endl;
+		for (int i = (rank + 0) * (n / numtasks + n % numtasks); i < (rank + 1) * (n / numtasks + n % numtasks) && i < n; i++) {
+			cout << "Call " << i << "For rank " << rank << endl;
+		}
+
+		for (int i = 0; i < n; i++)
+			delete[] matrix[i];
 		delete[] matrix;
 	}
 
